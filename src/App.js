5c8313d45756a311f52ab877f645710653a15668
Amir <amir.hackett@gmail.com>
2022-07-05 18:58:48 -0400

    added about and home components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLinks";
+import Socials from "./components/Socials";
 import Footer from "./components/Footer"
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
       <Experience />
       <Contact />
       <Footer />
-      <SocialLinks />
+      <Socials />
     </div>
   );
 }

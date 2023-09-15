7390497ad664e57e8f7082acc2b6c8a6fe207245
Amir <amir.hackett@gmail.com>
2023-09-15 08:34:31 -0400

    updated projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/book-search.jpg";
-import emotion from "../../Assets/Projects/noteTaker.jpg";
-import editor from "../../Assets/Projects/og-snkrs.jpg";
-import chatify from "../../Assets/Projects/password.jpg";
-import suicide from "../../Assets/Projects/pizza.jpg";
-import bitsOfCode from "../../Assets/Projects/recipe.jpg";
+import book from "../../Assets/Projects/book-search.jpg";
+import note from "../../Assets/Projects/noteTaker.jpg";
+import snkrs from "../../Assets/Projects/og-snkrs.jpg";
+import password from "../../Assets/Projects/password.jpg";
+import pizza from "../../Assets/Projects/pizza.jpg";
+import recipe from "../../Assets/Projects/recipe.jpg";
 
 function Projects() {
   return (
@@ -23,10 +23,10 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={password}
               isBlog={false}
               title="Book Search"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="A MERN stack application using Google Books API that allows a user to login, search books and save favorites."
               ghLink='https://github.com/Amir-Hackett/book-search'
               demoLink='https://ah-book-search.herokuapp.com/'
             />
@@ -34,10 +34,10 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={recipe}
               isBlog={false}
               title="Note Taker"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              description="An Express.js application for taking notes."
               ghLink='https://github.com/Amir-Hackett/deep-thoughts'
               demoLink='https://ah-deep-thoughts.herokuapp.com/'
             />
@@ -45,10 +45,10 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={snkrs}
               isBlog={false}
               title="OG Snkrs"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="This interactive SPA MERN stack web application utilizes HTML, CSS, JavaScript, MongoDB, Express, React, Node, GraphQL and Node Package Manager to build a responsive sneaker platform where sneakerheads and shoe enthusiasts have the ability to view latest releases, compare prices, and choose the best resale site option to purchase from."
               ghLink='https://github.com/Amir-Hackett/OG-SNKRS'
               demoLink='https://og-snkrs.herokuapp.com/'            
             />
@@ -56,21 +56,21 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={book}
               isBlog={false}
               title="Password Generator"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              description="Website used to Generate a Unique password with a minimum of 8 characters and a maximum of 128."
+              ghLink="https://github.com/Amir-Hackett/password-generator"
+              demoLink="https://amir-hackett.github.io/password-generator/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={pizza}
               isBlog={false}
               title="Pizza Search"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              description="A self-aware riff on social-media applications that already exist for other topics—lets users connect with each other based on their love of pizza."
               ghLink='https://github.com/Amir-Hackett/pizza-hunt'
               demoLink='https://amhpizza.herokuapp.com/'
             />
@@ -78,11 +78,10 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={note}
               isBlog={false}
               title="RecipEZ"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              description="An application that allows users to search for a recipe based on a recipe or a drink."
               ghLink='https://github.com/Amir-Hackett/RecipEZ'
               demoLink='https://amir-hackett.github.io/RecipEZ/'
             />
